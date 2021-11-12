5fd0409ca36ee37537918b459eac004dcab8fb56
Jordan <38331748+jrussell1@users.noreply.github.com>
2021-11-11 19:11:00 -0500

    Update bamazonCustomer1.js

--- bamazonCustomer1.js
+++ bamazonCustomer1.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "Raymondweil1",
+  password: "************ENTER PASSWORD*********************",
   database: "Bamazon"
 });
 
